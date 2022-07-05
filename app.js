2d6c719e08b29c4d44e91f5fc66d74fe524cc6e9
Zion <81079621+ryanzion777@users.noreply.github.com>
2022-07-05 22:11:29 +0800

    fix - cors

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var connectDB = require('./service/connectHandle');
-var appError = require('./service/appError');
-var apiMessage = require('./service/apiMessage');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var connectDB = require("./service/connectHandle");
+var appError = require("./service/appError");
+var apiMessage = require("./service/apiMessage");
 
-var authRouter = require('./routes/auth');
-var uploadRouter = require('./routes/upload');
-var indexRouter = require('./routes/index');
-var postRouter = require('./routes/posts');
-var userRouter = require('./routes/users');
-var likeRouter = require('./routes/likes');
-var commentRouter = require('./routes/comments');
-var followRouter = require('./routes/follows');
+var authRouter = require("./routes/auth");
+var uploadRouter = require("./routes/upload");
+var indexRouter = require("./routes/index");
+var postRouter = require("./routes/posts");
+var userRouter = require("./routes/users");
+var likeRouter = require("./routes/likes");
+var commentRouter = require("./routes/comments");
+var followRouter = require("./routes/follows");
 
 var app = express();
 
 // 程式出現重大錯誤時
-process.on('uncaughtException', err => {
+process.on("uncaughtException", (err) => {
   // 記錄錯誤下來，等到服務都處理完後，停掉該 process
-	console.error('Uncaughted Exception!!')
-	console.error(err);
-	process.exit(1);
+  console.error("Uncaughted Exception!!");
+  console.error(err);
+  process.exit(1);
 });
 
 // connect DB
 connectDB();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api', authRouter);
-app.use('/api', uploadRouter);
-app.use('/api', postRouter);
-app.use('/api', userRouter);
-app.use('/api', likeRouter);
-app.use('/api', commentRouter);
-app.use('/api', followRouter);
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/api", authRouter);
+app.use("/api", uploadRouter);
+app.use("/api", postRouter);
+app.use("/api", userRouter);
+app.use("/api", likeRouter);
+app.use("/api", commentRouter);
+app.use("/api", followRouter);
 
 // catch 404 找不到路由
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   return next(appError(apiMessage.ROUTER_NOT_FOUND, next));
 });
 
@@ -57,15 +60,15 @@ app.use(function(req, res, next) {
 const resErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).send({
-      message: err.message
+      message: err.message,
     });
   } else {
     // log 紀錄
-    console.error('出現重大錯誤', err);
+    console.error("出現重大錯誤", err);
     // 送出罐頭預設訊息
     res.status(500).send({
-      status: 'error',
-      message: '系統錯誤，請恰系統管理員!!'
+      status: "error",
+      message: "系統錯誤，請恰系統管理員!!",
     });
   }
 };
@@ -74,46 +77,46 @@ const resErrorDev = (err, res) => {
   res.status(err.statusCode).send({
     message: err.message,
     error: err,
-    stack: err.stack
+    stack: err.stack,
   });
 };
 
 // 底層錯誤處理 dev開發 || prod正式
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   // dev
-  if (process.env.NODE_ENV === 'dev') {
+  if (process.env.NODE_ENV === "dev") {
     return resErrorDev(err, res);
-  } 
+  }
   // production
-  if (err.name === 'ValidationError') {
+  if (err.name === "ValidationError") {
     err.statusCode = 400;
     err.message = "資料欄位未填寫正確，請重新輸入";
     err.isOperational = true;
     return resErrorProd(err, res);
-  } else if(err.name === 'CastError') {
+  } else if (err.name === "CastError") {
     err.statusCode = 400;
     err.message = "ＩＤ格式錯誤，請重新輸入";
     err.isOperational = true;
     return resErrorProd(err, res);
-  } else if(err.name === 'SyntaxError') {
+  } else if (err.name === "SyntaxError") {
     err.statusCode = 400;
     err.message = "語法不合法或代碼錯誤，請重新確認";
     err.isOperational = true;
     return resErrorProd(err, res);
-  } else if(err.name === 'ReferenceError') {
+  } else if (err.name === "ReferenceError") {
     err.statusCode = 400;
     err.message = "找不到參數數值，請重新確認";
     err.isOperational = true;
     return resErrorProd(err, res);
-  } 
+  }
 
   resErrorProd(err, res);
 });
 
-// promise 未捕捉到的 catch 
-process.on('unhandledRejection', (err, promise) => {
-  console.error('!!未捕捉到的 rejection：', promise, '原因：', err);
+// promise 未捕捉到的 catch
+process.on("unhandledRejection", (err, promise) => {
+  console.error("!!未捕捉到的 rejection：", promise, "原因：", err);
 });
 
 module.exports = app;
