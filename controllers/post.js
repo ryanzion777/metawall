933c4dc7fd0390fc6bfe98c6d6cac4cf0281bc3b
Zion <81079621+ryanzion777@users.noreply.github.com>
2022-07-19 21:17:29 +0800

    add post image

--- controllers/post.js
+++ controllers/post.js
@@ -1,161 +1,174 @@
 // Post Controller
-const Post = require('../models/Post');
-const User = require('../models/User');
-const successHandle = require('../service/successHandle');
-const catchAsync = require('../service/catchAsync');
-const appError = require('../service/appError');
-const apiMessage = require('../service/apiMessage');
+const Post = require("../models/Post");
+const User = require("../models/User");
+const successHandle = require("../service/successHandle");
+const catchAsync = require("../service/catchAsync");
+const appError = require("../service/appError");
+const apiMessage = require("../service/apiMessage");
 
 /*
   取得資料庫所有貼文 GET
 */
-const getPosts = catchAsync(async(req, res, next) => {
+const getPosts = catchAsync(async (req, res, next) => {
   // q => 搜尋項目
   // s => 資料排序
   const { q, s } = req.query;
-  const query = q ? { 'content': new RegExp(q) } : {};
-  const sort = s === 'hot' ? { likes: -1 } : s === 'new' ? '-createdAt' : 'createdAt';
+  const query = q ? { content: new RegExp(q) } : {};
+  const sort =
+    s === "hot" ? { likes: -1 } : s === "new" ? "-createdAt" : "createdAt";
 
   const data = await Post.find(query)
     .populate({
-      path: 'user',
-      select: 'name image'
+      path: "user",
+      select: "_id name avatar",
     })
     .populate({
-      path: 'comments'
+      path: "comments",
     })
     .sort(sort);
 
   successHandle({
-    res, message: '取得貼文成功', data
+    res,
+    message: "取得貼文成功",
+    data,
   });
 });
 
 /*
   取得單一貼文 GET
 */
-const getOnlyPost = catchAsync(async(req, res, next) => {
+const getOnlyPost = catchAsync(async (req, res, next) => {
   const { post_id } = req.params;
 
-  if(!post_id) {
+  if (!post_id) {
     return next(appError(apiMessage.FIELD_FAILED, next));
   }
 
-  const data = await Post
-    .find({  
-      _id: post_id
+  const data = await Post.find({
+    _id: post_id,
+  })
+    .populate({
+      path: "user",
+      select: "_id name avatar",
     })
     .populate({
-      path: 'user',
-      select: '_id name avatar',
+      path: "comments",
     });
 
-  if(!data) return next(appError(apiMessage.DATA_NOT_FOUND, next));
+  if (!data) return next(appError(apiMessage.DATA_NOT_FOUND, next));
 
   successHandle({
-    res, message: '取得單一貼文成功', data
+    res,
+    message: "取得單一貼文成功",
+    data,
   });
 });
 
 /*
   取得使用者所有貼文 GET
 */
-const getUserPosts = catchAsync(async(req, res, next) => {
+const getUserPosts = catchAsync(async (req, res, next) => {
   // q => 搜尋項目
   // s => 資料排序
   const { target_user_id } = req.params;
   const { q, s } = req.query;
-  const query = q ? { 'content': new RegExp(q) } : {};
-  const sort = s === 'hot' ? { likes: -1 } : s === 'new' ? '-createdAt' : 'createdAt';
+  const query = q ? { content: new RegExp(q) } : {};
+  const sort =
+    s === "hot" ? { likes: -1 } : s === "new" ? "-createdAt" : "createdAt";
 
   const data = await Post.find({ user: target_user_id, query })
     .populate({
-      path: 'user',
-      select: 'name avatar'
+      path: "user",
+      select: "name avatar",
     })
     .populate({
-      path: 'comments'
+      path: "comments",
     })
     .sort(sort);
 
   successHandle({
-    res, message: '取得使用者所有貼文成功', data
+    res,
+    message: "取得使用者所有貼文成功",
+    data,
   });
 });
 
 /*
   取得使用者按讚的貼文 GET
 */
-const getPostLikes = catchAsync(async(req, res, next) => {
+const getPostLikes = catchAsync(async (req, res, next) => {
   const { target_user_id } = req.params;
 
-  if(!target_user_id) {
+  if (!target_user_id) {
     return next(appError(apiMessage.FIELD_FAILED, next));
   }
 
-  const data = await Post
-    .find({  
-      likes: { $in: [target_user_id] }
-    })
-    .populate({
-      path: 'user',
-      select: 'name avatar',
-    });
+  const data = await Post.find({
+    likes: { $in: [target_user_id] },
+  }).populate({
+    path: "user",
+    select: "name avatar",
+  });
 
-  if(!data) return next(appError(apiMessage.DATA_NOT_FOUND, next));
+  if (!data) return next(appError(apiMessage.DATA_NOT_FOUND, next));
 
   successHandle({
-    res, message: '取得使用者按讚的貼文成功', data
+    res,
+    message: "取得使用者按讚的貼文成功",
+    data,
   });
 });
 
 /*
   上傳單一貼文 POST
 */
-const createPost = catchAsync(async(req, res, next) => {
+const createPost = catchAsync(async (req, res, next) => {
   const { content, images, likes } = req.body;
   const user_id = req.user_id;
 
-  if(!content || !user_id) {
+  if (!content || !user_id) {
     return next(appError(apiMessage.FIELD_FAILED, next));
   }
 
   const data = await Post.create({
-    user: user_id, 
-    content, 
-    images, 
-    likes
+    user: user_id,
+    content,
+    images,
+    likes,
   });
 
   successHandle({
-    res, message: '上傳貼文成功', data
+    res,
+    message: "上傳貼文成功",
+    data,
   });
 });
 
 /*
   更新單一貼文 PATCH
 */
-const updatePost = catchAsync(async(req, res, next) => {
+const updatePost = catchAsync(async (req, res, next) => {
   const { post_id } = req.params;
   const { content, images } = req.body;
   const user_id = req.user_id;
 
-  if(!post_id || !content || !user_id) {
+  if (!post_id || !content || !user_id) {
     return next(appError(apiMessage.FIELD_FAILED, next));
   }
 
   const post = await Post.findById(post_id);
 
-  if(post) {
+  if (post) {
     const nowPatch = {};
-    if(images) nowPatch.images = images;
-    if(content) nowPatch.content = content;
+    if (images) nowPatch.images = images;
+    if (content) nowPatch.content = content;
     const data = await Post.findByIdAndUpdate(post_id, nowPatch, { new: true });
     successHandle({
-      res, message: '更新貼文成功', data
+      res,
+      message: "更新貼文成功",
+      data,
     });
-  }
-  else {
+  } else {
     return next(appError(apiMessage.DATA_NOT_FOUND, next));
   }
 });
@@ -163,23 +176,24 @@ const updatePost = catchAsync(async(req, res, next) => {
 /*
   刪除單一貼文 DELETE
 */
-const deletePost = catchAsync(async(req, res, next) => {
+const deletePost = catchAsync(async (req, res, next) => {
   const { post_id } = req.params;
   const user_id = req.user_id;
-  
-  if(!post_id || !user_id) {
+
+  if (!post_id || !user_id) {
     return next(appError(apiMessage.FIELD_FAILED, next));
   }
 
   const post = await Post.findById(post_id);
 
-  if(post) {
+  if (post) {
     const data = await Post.findByIdAndDelete(post_id);
     successHandle({
-      res, message: '刪除單一貼文成功', data
+      res,
+      message: "刪除單一貼文成功",
+      data,
     });
-  }
-  else {
+  } else {
     return next(appError(apiMessage.DATA_NOT_FOUND, next));
   }
 });
@@ -187,15 +201,23 @@ const deletePost = catchAsync(async(req, res, next) => {
 /*
   刪除所有貼文 DELETE
 */
-const deletePosts= catchAsync(async(req, res, next) => {
+const deletePosts = catchAsync(async (req, res, next) => {
   await Post.deleteMany({});
   const data = await Post.find();
   successHandle({
-    res, message: '刪除所有貼文成功', data
+    res,
+    message: "刪除所有貼文成功",
+    data,
   });
 });
 
-
 module.exports = {
-  getPosts, getUserPosts, getOnlyPost, getPostLikes, createPost, updatePost, deletePost, deletePosts
+  getPosts,
+  getUserPosts,
+  getOnlyPost,
+  getPostLikes,
+  createPost,
+  updatePost,
+  deletePost,
+  deletePosts,
 };
